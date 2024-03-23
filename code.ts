c813fe187aea6e42f2a42e3fd36ec6f73107285a
salahu01 <cvsalahu12@gmail.com>
2024-03-23 17:07:27 +0530

    added new ui

--- code.ts
+++ code.ts
@@ -6,36 +6,52 @@ let uniqueFontSizes: Set<number> = new Set();
 
 // Function to handle messages from the UI
 figma.ui.onmessage = (msg) => {
-  if (msg.type === 'generate') {
+  if (msg.type === "generate") {
     const unwantedNames: string[] = msg.unwantedNames || [];
-    console.log(unwantedNames)
+    console.log(unwantedNames);
     uniqueFontSizes.clear(); // Clear the set before populating it again
-    figma.currentPage.selection.forEach(node => {
-      console.log(node.type)
-      findSizes(node, unwantedNames)
+    figma.currentPage.selection.forEach((node) => {
+      console.log(node.type);
+      findSizes(node, unwantedNames);
     });
 
     // Generate static text styles for each font size
-    const textStyles = Array.from(uniqueFontSizes).map(fontSize => `static TextStyle get text${fontSize}Px => const TextStyle(fontSize: ${fontSize});`);
+    const textStyles = Array.from(uniqueFontSizes).map(
+      (fontSize) =>
+        `static TextStyle get text${fontSize}Px => const TextStyle(fontSize: ${fontSize});`
+    );
 
     // Send generated text styles back to the UI
-    figma.ui.postMessage({ type: 'textStyles', textStyles });
+    figma.ui.postMessage({ type: "textStyles", textStyles });
   }
 };
 
 function findSizes(node: SceneNode, unwantedNames: string[]) {
-
   if (unwantedNames.includes(node.name)) {
-    return
-  } else if (node.type === 'TEXT') {
+    return;
+  } else if (node.type === "TEXT") {
     // Add font size to the set
-    if (typeof (node as TextNode).fontSize === 'number') {
-      uniqueFontSizes.add((node as TextNode).fontSize as number)
+    if (typeof (node as TextNode).fontSize === "number") {
+      uniqueFontSizes.add((node as TextNode).fontSize as number);
     }
-
-  } else if (node.type === 'FRAME' || node.type === 'SECTION' || node.type === 'GROUP' || node.type === "COMPONENT_SET" || node.type === "INSTANCE" || node.type === "BOOLEAN_OPERATION") {
+  } else if (
+    node.type === "FRAME" ||
+    node.type === "SECTION" ||
+    node.type === "GROUP" ||
+    node.type === "COMPONENT_SET" ||
+    node.type === "INSTANCE" ||
+    node.type === "BOOLEAN_OPERATION"
+  ) {
     // Iterate through children of the frame
-    (node as FrameNode | SectionNode | GroupNode | ComponentSetNode | InstanceNode | BooleanOperationNode).children.forEach(child => {
+    (
+      node as
+        | FrameNode
+        | SectionNode
+        | GroupNode
+        | ComponentSetNode
+        | InstanceNode
+        | BooleanOperationNode
+    ).children.forEach((child) => {
       findSizes(child, unwantedNames); // Recursive call for each child node
     });
   }
